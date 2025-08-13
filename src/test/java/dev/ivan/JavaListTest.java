410ace98ed93dd74df645491ce6a4cb157fe9237
Ivan Lorenzo <elhibar77@hotmail.es>
2025-08-13 11:57:57 +0200

    refact: modificado test para la creación de la lista

--- src/test/java/dev/ivan/JavaListTest.java
+++ src/test/java/dev/ivan/JavaListTest.java
@@ -6,46 +6,28 @@ import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import java.util.List;
-import java.io.ByteArrayOutputStream;
+
 import java.io.PrintStream;
 
 import org.junit.jupiter.api.Test;
 
-
-
 public class JavaListTest {
 
 @Test
- public void testCreateDayList() {
+public void testCreateDayList() {
     JavaList javaList = new JavaList();
-    List<String> days = javaList.dayListCreation();
-    assertThat(days.size(), is(equalTo(7)));
-    assertThat(days, contains(
-        "Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"
-    ));
- }
     
-@Test
-    void testPrintDays() {
 
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
+    javaList.dayListCreation();
+    
+    List<String> days = javaList.getDays();
 
-        JavaList javaList = new JavaList();
-        javaList.printDays();
+    assertThat(days.size(), is(equalTo(7)));
 
-        System.setOut(System.out);
+    assertThat(days, contains(
+            "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"
+    ));
+}
 
-        String[] outputLines = outContent.toString().trim().split("\\R");
 
-        assertThat(outputLines, arrayContaining(
-                "Lunes",
-                "Martes",
-                "Miércoles",
-                "Jueves",
-                "Viernes",
-                "Sábado",
-                "Domingo"
-        ));
-    }
 }
